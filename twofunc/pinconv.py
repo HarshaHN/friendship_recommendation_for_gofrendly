1705af1b94eeb6161714186c67f443f2da5084c5
Harsha HN <harshahn@kth.se>
2020-05-26 22:23:42 +0200

    train works! trian.py and pinconv.py

--- twofunc/pinconv.py
+++ twofunc/pinconv.py
@@ -1,25 +1,29 @@
-"""Torch modules for pinsage graph convolutions(PGCN)."""
+"""
+Date: 18 May 2020
+Goal: 01 Pinsage algo 1 for one node.
+Author: Harsha HN harshahn@kth.se
+"""
 
+#%% ------------------------
+""" 00. import libraries """
 import torch as th
 from torch import nn
 from torch.nn import init
 import dgl.function as fn
 import torch.nn.functional as F
-#from ....base import DGLError
+#dgl.load_backend('pytorch')
 
+#%%-----------------------------------------
+""" 01. PinsageConv """
 class PinConv(nn.Module):
    
- def __init__(self,
-                 in_feats,
-                 hidden_feats,
-                 out_feats,
-                 norm='right'):
+    def __init__(self, in_feats, hidden_feats, out_feats):
         super(PinConv, self).__init__()
+        # feature size
         self._in_feats = in_feats
         self._hidden_feats = hidden_feats
-        self._out_feats = out_feats
-        self._norm = norm
-
+        self._out_feats = out_feats 
+        
         # kernels
         self.Q = nn.Linear(in_feats, hidden_feats)
         self.W = nn.Linear(in_feats + hidden_feats, out_feats)
@@ -32,27 +36,17 @@ class PinConv(nn.Module):
     def forward(self, graph, feat):
         """Compute graph convolution.
 
-        Notes
-        -----
-        * Input shape: :math:`(N, *, \text{in_feats})` where * means any number of additional
-          dimensions, :math:`N` is the number of nodes.
-        * Output shape: :math:`(N, *, \text{out_feats})` where all but the last dimension are
-          the same shape as the input.
-        * Weight shape: "math:`(\text{in_feats}, \text{out_feats})`.
-
         Parameters
         ----------
         graph : DGLGraph
             The graph.
         feat : torch.Tensor
-            The input feature
-        weight : torch.Tensor, optional
-            Optional external weight tensor.
+            The input feature (Node embeddings)
 
         Returns
         -------
         torch.Tensor
-            The output feature
+            The output feature (Node embeddings)
         """
         graph = graph.local_var()
 
@@ -62,14 +56,17 @@ class PinConv(nn.Module):
         rst = graph.dstdata['h']
         rst = th.cat([feat, rst], 1)
         rst = F.relu(self.W(rst))
-
+        
         degs = graph.in_degrees().to(feat.device).float().clamp(min=1)
         norm = 1.0 / degs
         shp = norm.shape + (1,) * (feat.dim() - 1)
         norm = th.reshape(norm, shp)
         rst = rst * norm
 
-        rst = rst / rst.norm(dim=1, keepdim=True)
+        denom = rst.norm(dim=1, keepdim=True)
+        if any(denom) != 0:
+            rst = rst / denom
+
         return rst
 
     def extra_repr(self):
--- twofunc/train.py
+++ twofunc/train.py
@@ -5,27 +5,35 @@ Goal: 02 Create a sample minibatch and algo 2 for minibatches. (try to do it in 
 Author: Harsha HN harshahn@kth.se
 """
 
-#%% ------------------------
-""" 00. import libraries """
+#%%---------------
+# import torch
 import torch
-import itertools
 import torch.nn as nn
 import torch.nn.functional as F
+import pinconv
+import matplotlib.pyplot as plt 
+import itertools
 import dgl
 from dgl.data.utils import save_graphs, load_graphs
-from pinsage import PinSageConv 
-import matplotlib.pyplot as plt 
+from importlib import reload; reload(pinconv)
 
 #%%-----------------------------------------
-""" 01. Load the graph data """
-"""
-G = load_graphs("../data/two/dglmf_noniso.bin")[0][0] 
-print('We have %d nodes' % G.number_of_nodes(), 'with %d edges' % (G.number_of_edges()/2)) 
-del G.ndata['id']
-model = PinSageConv(in_features=2, out_features=2, hidden_features=2)
 
-"""
-"""01. Dummy graph data"""
+def nxviz(G):
+    import networkx as nx
+    nx_G = G.to_networkx()
+    pos = nx.kamada_kawai_layout(nx_G)
+    nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]])
+
+def getG():
+    #g.add_edges(g.nodes(), g.nodes())
+    G = load_graphs("../data/two/dglmf_noniso.bin")[0][0] 
+    print('We have %d nodes' % G.number_of_nodes(), 'with %d edges' % (G.number_of_edges()/2)) 
+    del G.ndata['id']
+    G = dgl.transform.to_bidirected(G)
+    G.readonly(True)
+    return G
+
 def getT():
     T = dgl.DGLGraph()
     T.add_nodes(5)
@@ -34,138 +42,82 @@ def getT():
     T.readonly(True)
     return T
 
-def nxviz(G):
-    import networkx as nx
-    nx_G = G.to_networkx()#.to_undirected()
-    pos = nx.kamada_kawai_layout(nx_G)
-    nx.draw(nx_G, pos, with_labels=True, node_color=[[.7, .7, .7]])
+
+def getlinks():
+    pos = torch.tensor([[0], [1]], requires_grad=False) #mf
+    neg = torch.tensor([[0], [3]], requires_grad=False) #bf
+    return [pos, neg]
 
 #nxviz(getT())
 
+#%%------------------
+#Define a Graph Convolution Network
+class PCN(nn.Module):
+    def __init__(self, in_feats, hidden_size, out_feats, K=2):
+        super(PCN, self).__init__()
+        # kernels
+        self.pinconvs = nn.ModuleList([
+            pinconv.PinConv(in_feats, hidden_size, out_feats) for k in range(K) ])
+        self.G = nn.Linear(out_feats, out_feats)
+        nn.init.xavier_uniform_(self.G.weight, gain=nn.init.calculate_gain('relu'))
+        nn.init.constant_(self.G.bias, 0)
 
+    def forward(self, g, inputs):
+        h = inputs
+        for i, pconv in enumerate(self.pinconvs):
+            h = pconv(g, h)
+        h = F.relu(self.G(h))
+        return h
 
-#%%-------------------------------
-T = getT()
-K = 2
-S = list()
-S = S.extend([list()*K, list(T.nodes())])
+#%%-------------------------
+#Hyper parameters
+fdim = 8
+fsize = [fdim]*3
+layers = 2
+lr = 5e-4
+epochs = 100*2
+margin = .0 #loss
 
-
-
-
-
-
-
-
-
-
-
-
-#%%------------------------------
-""" 02. PinSAGE minibatch """
-
-import pinsage
-from importlib import reload; reload(pinsage)
-class minibatch:
-    def __init__(self, T, inputs):
-        self.T = T
-        self.T.ndata['feat'] = inputs
-        fsize = [inputs.shape[1]] * 3
-        self.model = pinsage.PinSageConv(*fsize)
-
-    def run(self):
-        for node in self.T.nodes():
-            self.T.apply_nodes(func=self.onelayergcn, v=[node])
-        return self.T.ndata['feat']
-
-    def onelayergcn(self, node): #nodebatch
-        # sub-graph about the node
-        n = dgl.contrib.sampling.sampler.NeighborSampler(g=self.T, batch_size=1, expand_factor=-1, num_hops=1, neighbor_type='in', transition_prob=None, seed_nodes=node.nodes(), shuffle=False, num_workers=1, prefetch=False, add_self_loop=False)
-        # get the nodeflow
-        nf = n.fetch(current_nodeflow_index=0)[0]
-        # copy the feature data from the parent graph
-        nf.copy_from_parent()
-        # get the node feature
-        h_node = nf.layers[1].data['feat'][0]
-        # get the node feature of its neighbors
-        h_ngbrs = nf.layers[0].data['feat']
-        # alpha or neighbor weights
-        alpha = torch.ones_like(h_ngbrs)
-        # perform pinconv and get new nodes features for the layers i+1
-        node_newfeat = self.model(h_node, h_ngbrs, alpha, gamma='mean')
-        return {'feat': node_newfeat[None]}
-
-#%%------------------------------
-""" 02. PinSAGE train """
-# positive and negative links ([0,0,1,2,3], [1,2,3,4,4])
-pos = torch.tensor([[0,1,2], [1,3,4]], requires_grad=False)
-neg = torch.tensor([[0,3], [2,4]], requires_grad=False)
+# Get the graph
+G = getT() # getG()
+embed = nn.Embedding(G.number_of_nodes(), fdim)
+nemb = embed.weight
+G.ndata['feat'] = embed.weight #embed.weight = G.ndata['feat']
 
 # Define the model
-fsize = 20
-mb = minibatch(getT(), inputs=nn.Embedding(5, fsize).weight) 
+net = PCN(*fsize, layers)
 
-# Define the optimizer
-optimizer = torch.optim.Adam(mb.model.parameters(), lr=1e-5)
+# Get links
+[pos, neg] = getlinks()
 
 # Define the loss func
-def lossfunc(newemb):
+def lossfunc(newemb, margin):
     p = torch.mul(newemb[pos[0]], newemb[pos[1]]).sum(1).mean()
     n = torch.mul(newemb[neg[0]], newemb[neg[1]]).sum(1).mean()
-    margin = 1.0
-    return F.relu(n-p + margin)#.clamp(min=0) - p + margin
+    return F.relu(n - p + margin)
 
-"""
-import time
-print(list(mb.model.parameters())[0], '\n')
-print('Training begins ...')
-print('Loss: ', lossfunc(mb.T.srcdata['feat']).item())
-print('--> mb.T:', mb.T.srcdata['feat'], '\n')
-"""
-#%%
-loss_values = [0]
-for epoch in range(150):
-    # Generate new embeddings
-    newemb = mb.run()
-    # Compute the loss
-    loss = lossfunc(newemb)
+# Define the optimizer
+optimizer = torch.optim.Adam(itertools.chain(net.parameters(), embed.parameters()), lr)
+loss_values = []
+
+#%%---------------------------------
+epochs = 100*5
+for epoch in range(epochs):
+    newemb = net(G, embed.weight)
+    loss = lossfunc(newemb, margin)
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    newemb.detach_()
-    
+
+    print('Epoch %d | Loss: %.4f' % (epoch, loss.item()))
     loss_values.append(loss.item())
-
-    #print('Epoch %d | Loss: %.4f' % (epoch, loss.item()))
-    #time.sleep(0.2)
-    #print(mb.T.srcdata['feat'], '\n')
-    #print('Grad: \n', list(mb.model.parameters())[0].grad)
-    #print('--> newemb:', newemb)
-
+    if loss.item() < 0.05: break
+    
 plt.plot(loss_values)
+#list(net.parameters())[0].grad
 
-#%%
-"""    
-    logits = net(G, inputs)
-    # we save the logits for visualization later
-    all_logits.append(logits.detach())
-    logp = F.log_softmax(logits, 1)
-    # we only compute loss for labeled nodes
-    loss = F.nll_loss(logp[labeled_nodes], labels)
-
-###########################################3333
-
-Assumptions: as far as the operations are sequentially arranged, they are trainable.
-To-do: scale for K layers, encapsulate the vars, 
-
-Helpers:
-"""
-
-#%%-------------------------------------------
-""" Utility ops"""
-def clr():
-    import sys
-    sys.modules[__name__].__dict__.clear()
-
+#%%-----------
+import sys
+sys.modules[__name__].__dict__.clear()
 
